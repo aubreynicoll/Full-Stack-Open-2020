ee18af8404c2340258dcd92db8e4b2a217e20e0d
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-05-14 14:22:11 +1000

    Finish exercise 1.6: unicafe step 1

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const Heading = ({text}) =>
+    <h1>{text}</h1>
+
+const Button = ({onClick, text}) =>
+    <button onClick={onClick}>{text}</button>
+
+const Display = ({name, value}) =>
+    <div>{name}: {value}</div>
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const incrementState = (state, setter) =>
+        setter(state + 1)
+
+    return(
+        <div>
+            <Heading text='Give Feedback' />
+            <Button 
+                onClick={() => incrementState(good, setGood)}
+                text='Good' />
+            <Button
+                onClick={() => incrementState(neutral, setNeutral)}
+                text='Neutral' />
+            <Button
+                onClick={() => incrementState(bad, setBad)}
+                text='Bad' />
+            <Heading text='Statistics' />
+            <Display name='Good' value={good} />
+            <Display name='Neutral' value={neutral} />
+            <Display name='Bad' value={bad} />
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
