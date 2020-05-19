060a075f6494df7f9760411aec347d899a3eddf3
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-05-19 21:55:35 +1000

    finish exercise 2.5

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -3,29 +3,54 @@ import ReactDOM from 'react-dom';
 import Course from './components/Course';
 
 const App = () => {
-    const course = { 
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using Props to Pass Data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a Component',
-                exercises: 14,
-                id: 3
-            }
-        ]
-    }
+    const courses = [
+        { 
+            id: 1,
+            name: 'Half Stack application development',
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using Props to Pass Data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a Component',
+                    exercises: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: 'Node.js',
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middleware',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
-    return <Course course={course} />
+
+    return (
+        <div>
+            {courses.map((course) =>
+                <Course key={course.id} course={course} />
+            )}
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
