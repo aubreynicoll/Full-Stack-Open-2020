d8d43cbb89e011d892e244928f4f76fe4090379c
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-06-23 19:33:14 +1000

    finish exercises 4.1, 4.2

--- part3/phonebook backend/models/person.js
+++ part3/phonebook backend/models/person.js
@@ -9,13 +9,13 @@ mongoose.set('useCreateIndex', true)
 const url = process.env.MONGODB_URI
 console.log('connecting to MongoDB...')
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then(response => {
-    console.log('connected to MongoDB!')    
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB!')
   })
   .catch(error => {
     console.log('connection to MongoDB failed:', error.message)
-  }) 
+  })
 
 // define schema
 const personSchema = mongoose.Schema({
