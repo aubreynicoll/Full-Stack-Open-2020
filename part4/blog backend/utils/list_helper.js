0bde6a85008fcfa120ee0276b92c506e7782ad19
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-06-24 12:15:51 +1000

    finish exercise 4.5

--- part4/blog backend/utils/list_helper.js
+++ part4/blog backend/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? undefined
+    : blogs.reduce((rsf, blog) => rsf.likes >= blog.likes ? rsf : blog)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
