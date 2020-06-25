c482bb6443f32a394b643b1bd4e38dfb4f6eec89
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-06-25 15:26:04 +1000

    finish exercise 4.7

--- part4/blog backend/utils/list_helper.js
+++ part4/blog backend/utils/list_helper.js
@@ -16,18 +16,40 @@ const favoriteBlog = (blogs) => {
 }
 
 const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined
+  }
+
   blogs = _.countBy(blogs, 'author')
   blogs = _.toPairs(blogs)
   blogs = blogs.map(blog => {return { author: blog[0], blogs: blog[1] }})
 
-  return blogs.length === 0
-    ? undefined
-    : blogs.reduce((rsf, blog) => rsf.blogs >= blog.blogs ? rsf : blog)
+  return blogs.reduce((rsf, blog) => rsf.blogs >= blog.blogs ? rsf : blog)
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined
+  }
+
+  let results = []
+
+  blogs.forEach(blog => {
+    if (results.length === 0 || !results.some(result => result.author === blog.author)) {
+      results = results.concat({ author: blog.author, likes: blog.likes })
+    }
+    else {
+      results = results.map(result => {return result.author === blog.author ? { author: result.author, likes: result.likes += blog.likes } : result})
+    }
+  })
+
+  return results.reduce((rsf, result) => rsf.likes >= result.likes ? rsf : result)
 }
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
