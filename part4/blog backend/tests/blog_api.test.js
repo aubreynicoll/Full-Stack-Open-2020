9ed9d4b14609c7a465cdbfb0c275c87d4f288d5e
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-07-01 14:42:56 +1000

    finish exercise 4.17

--- part4/blog backend/tests/blog_api.test.js
+++ part4/blog backend/tests/blog_api.test.js
@@ -2,7 +2,6 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 const app = require('../app')
 const api = supertest(app)
-const Blog = require('../models/blog')
 const helper = require('./test_helper')
 
 
