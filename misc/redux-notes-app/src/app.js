b17b9e8b00a43411b43f6d312b8e47825f730f41
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-11-12 13:19:41 +1000

    finish exercise 6.3

---
+++ misc/redux-notes-app/src/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NewNoteForm from './components/newNoteForm'
+import Notes from './components/Notes'
+
+
+const App = () => {
+  return (
+    <div>
+      <NewNoteForm />
+      <Notes />      
+    </div>
+  )
+}
+
+export default App
--- misc/redux-notes-app/src/reducers/noteReducer.js
+++ misc/redux-notes-app/src/reducers/noteReducer.js
@@ -15,4 +15,26 @@ const noteReducer = (state = [], action) => {
   }
 }
 
+const generateId = () => {
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+}
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTTANCE',
+    data: { id }
+  }
+}
+
 export default noteReducer
