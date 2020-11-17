0b51a30de6116ca360973cbe21519a847b13233b
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-11-17 12:08:06 +1000

    finish exercise 7.1

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,39 +1,35 @@
-const initialState = {
-  message: null,
-  timeoutId: null
-}
+const initialState = null
 
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_NOTIFICATION':
-      state.timeoutId && window.clearTimeout(state.timeoutId)
-      return action.data
+      return action.notification
     default:
       return state
   }
 }
 
+let timeoutId
+
 export const setNotification = (string, seconds) => {
   return async (dispatch) => {
+    timeoutId && clearTimeout(timeoutId)
+
     dispatch({
       type: 'SET_NOTIFICATION',
-      data: {
-        message: string,
-        timeoutId: setTimeout(() => {
-          dispatch(removeNotification())
-        }, seconds * 1000)
-      }
+      notification: string
     })
+
+    timeoutId = setTimeout(() => {
+      dispatch(removeNotification())
+    }, seconds * 1000)
   }
 }
 
 export const removeNotification = () => {
   return {
     type: 'SET_NOTIFICATION',
-    data: {
-      message: null,
-      timeoutId: null
-    }
+    notification: null
   }
 }
 
