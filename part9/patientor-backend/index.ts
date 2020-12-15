0b5b4a9b05b04fe0adb27d89bd22c1a15a312679
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2020-12-15 13:13:59 +1000

    finish exercise 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   res.send('pong!');
 });
 
